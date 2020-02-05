da882ad1b1b30909d94e336b25715aba4c596a02
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2020-02-05 17:55:59 +0100

    Added a very simple controller.

--- src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
+++ src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -3,6 +3,12 @@ package masterSpringMvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * This annotation combines three other ones
+ * @Configuration ---> indicates that our class will handle classical aspects of a Spring configuration.
+ * @ComponentScan ---> it tells Spring where to look to find our Spring components.
+ * @EnableAutoConfiguration ---> specific for Spring Boot.
+ */
 @SpringBootApplication
 public class MasterSpringMvcApplication {
 
