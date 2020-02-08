84641d67710ac64eafa764b9687ceb82e94248d5
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2020-02-08 01:18:26 +0100

    A request parameter is used.

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -3,14 +3,15 @@ package masterSpringMvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller   // @RestController = @Controller + @ResponseBody
 public class HelloController {
 	
 	@RequestMapping("/")
-	public String hello(Model model) {
+	public String hello(@RequestParam(defaultValue = "world") String name, Model model) {
 		System.out.println("===============Inside the controller==============");
-		model.addAttribute("message", "Hello from the controller");
+		model.addAttribute("message", "Hello, " + name);
 		return "resultPage";
 	}
 
