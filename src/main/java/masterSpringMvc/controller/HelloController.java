6d3e936676ca81af036fc2ddc2977b5315f79624
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2020-02-08 01:06:57 +0100

    A message from the model is displayed.

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -1,14 +1,16 @@
 package masterSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller   // @RestController = @Controller + @ResponseBody
 public class HelloController {
 	
 	@RequestMapping("/")
-	public String hello() {
+	public String hello(Model model) {
 		System.out.println("===============Inside the controller==============");
+		model.addAttribute("message", "Hello from the controller");
 		return "resultPage";
 	}
 
