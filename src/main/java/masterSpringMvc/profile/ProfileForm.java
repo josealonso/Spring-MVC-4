79dd9dfcd4f3230d8119e9dbf00633498f0faa4c
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2020-02-17 20:25:04 +0100

    Added basic validation to the form.

--- src/main/java/masterSpringMvc/profile/ProfileForm.java
+++ src/main/java/masterSpringMvc/profile/ProfileForm.java
@@ -4,11 +4,26 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 // Data Transfer Object (DTO)
+@SuppressWarnings("deprecation")
 public class ProfileForm {
-    private String twitterHandle;
+    @Size(min = 2)
+	private String twitterHandle;
+    
+    @Email
+    @NotEmpty
     private String email;
+    
+    @NotNull
     private LocalDate birthDate;
+    
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 	
     public String getTwitterHandle() {
